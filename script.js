108e0d1d5f45e22b47395e881ce409ee7e973539
150b3l <miguelvulcanisneves@gmail.com>
2025-05-12 00:13:01 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+function generateStatic() {
+    const canvas = document.createElement("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    const ctx = canvas.getContext("2d");
+
+    function draw() {
+        const imageData = ctx.createImageData(canvas.width, canvas.height);
+        for (let i = 0; i < imageData.data.length; i += 4) {
+            const shade = Math.random() * 255;
+            imageData.data[i] = shade;      // R
+            imageData.data[i + 1] = shade;  // G
+            imageData.data[i + 2] = shade;  // B
+            imageData.data[i + 3] = 255;    // Alpha
+        }
+        ctx.putImageData(imageData, 0, 0);
+    }
+
+    document.querySelector(".tv-static").appendChild(canvas);
+
+    setInterval(draw, 33);
+}
+
+window.onload = generateStatic;
